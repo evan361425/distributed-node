7e65a7bd007d2da405bb96c6768f2aada1396838
Shueh Chou Lu <wu0dj2k7ao3@gmail.com>
2021-04-20 13:36:32 +0800

    Cluster

---
+++ src/recipe-api/producer-cluster-fork.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+
+const HOST = process.env.HOST || '127.0.0.1';
+const PORT = parseInt(process.env.PORT || '4000');
+
+console.log(`worker pid=${process.pid}`);
+
+const app = express();
+app.get('/recipes/:id', async (req, res) => {
+  console.log(`worker request pid=${process.pid}`);
+  const id = Number(req.params.id);
+  if (id !== 42) {
+    res.status(404).send({ error: 'not_found' });
+  }
+  res.send({
+    producer_pid: process.pid,
+    recipe: {
+      id,
+      name: 'Chicken Tikka Masala',
+      steps: 'Throw it in a pot...',
+      ingredients: [
+        { id: 1, name: 'Chicken', quantity: '1 lb' },
+        { id: 2, name: 'Sauce', quantity: '2 cups' },
+      ],
+    },
+  });
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Producer running at http://${HOST}:${PORT}`);
+});
---
+++ src/recipe-api/producer-cluster-master.ts
@@ -0,0 +1,33 @@
+/**
+ * commands:
+ * 1. npx ts-node src/recipe-api/producer-cluster-master.ts
+ * 2.
+ *   1. curl http://localhost:4000/recipes/42
+ *   2. curl http://localhost:4000/recipes/42
+ *   3. curl http://localhost:4000/recipes/42
+ * 3. kill <PID worker 1>
+ * 4.
+ *   1. curl http://localhost:4000/recipes/42
+ *   2. curl http://localhost:4000/recipes/42
+ * 5. kill <PID worker 2>
+ */
+import cluster from 'cluster';
+
+console.log(`master pid=${process.pid}`);
+cluster.setupMaster({ exec: __dirname + '/producer-cluster-fork.ts' });
+
+// fork two cluster
+cluster.fork();
+cluster.fork();
+
+cluster
+  .on('disconnect', (worker) => {
+    console.log('disconnect', worker.id);
+  })
+  .on('exit', (worker, code, signal) => {
+    console.log('exit', worker.id, code, signal);
+    // cluster.fork();
+  })
+  .on('listening', (worker, { address, port }) => {
+    console.log('listening', worker.id, `${address}:${port}`);
+  });
