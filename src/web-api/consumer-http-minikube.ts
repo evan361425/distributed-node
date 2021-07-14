56f92b1438ccb2c54cdb0345dc7a180815bc76d3
Shueh Chou Lu <wu0dj2k7ao3@gmail.com>
2021-07-14 14:34:54 +0800

    for v2 demonstration

---
+++ src/web-api/consumer-http-minikube.ts
@@ -0,0 +1,27 @@
+/**
+ * Demonstrate kubernetes rebuilt when unhealthy
+ * 1. proxy - HA proxy - compression, tls
+ */
+import express from 'express';
+import got from 'got';
+
+const HOST = process.env.HOST || '127.0.0.1';
+const PORT = Number(process.env.PORT || 3000);
+const TARGET = process.env.TARGET || 'localhost:4000';
+
+const app = express();
+app.get('/', async (_req, res) => {
+  const producer_data = await got(`http://${TARGET}/recipes/42`).json();
+
+  res.json({
+    consumer_pid: process.pid,
+    producer_data,
+  });
+});
+app.get('/kill', () => {
+  process.exit(42);
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Consumer running at http://${HOST}:${PORT}/`);
+});
